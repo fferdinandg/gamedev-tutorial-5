317717e5dc5d603a06f22920e810d5b7e2dd3033
unknown <frferdinand22@gmail.com>
2024-03-13 13:04:25 +0700

    implement bgm and walk audio

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -5,6 +5,8 @@ export (int) var GRAVITY = 1200
 export (int) var jump_speed = -600
 export (int) var jump_count = 0
 const MAX_JUMP_COUNT = 2
+onready var _animated_sprite = $AnimatedSprite
+onready var walk_sound = $WalkAudio
 # implementasi double jump dari sini https://www.youtube.com/watch?v=f0r3EgjQNak
 
 
@@ -14,6 +16,7 @@ var velocity = Vector2()
 
 func get_input():
 	velocity.x = 0
+		
 	if jump_count < MAX_JUMP_COUNT and Input.is_action_just_pressed('ui_up'):
 		velocity.y = jump_speed
 		jump_count += 1;
@@ -22,10 +25,34 @@ func get_input():
 		jump_count +=2; # agar saat jatuh, hanya bisa double jump sekali
 	if Input.is_action_pressed('ui_right'):
 		velocity.x += speed
+		_animated_sprite.play("walk")
 	if Input.is_action_pressed('ui_left'):
 		velocity.x -= speed
+		_animated_sprite.play("walk")
+	if Input.is_action_pressed("ui_down"):
+		pass
 	if is_on_floor():
 		jump_count = 0
+	
+#		_animated_sprite.play("idle")
+#		_animated_sprite.stop()
+		
+	if velocity.x < 0:
+		_animated_sprite.flip_h = true
+	if velocity.x == 0:
+		_animated_sprite.play("idle")
+	if velocity.x > 0:
+		_animated_sprite.flip_h = false
+	if velocity.y < 0:
+		_animated_sprite.play("jump")
+	if velocity.x != 0 and is_on_floor():
+		if !walk_sound.playing:
+			walk_sound.play()
+	elif walk_sound.playing:
+		walk_sound.stop()
+		
+		
+	
 func _physics_process(delta):
 	velocity.y += delta * GRAVITY
 	get_input()
